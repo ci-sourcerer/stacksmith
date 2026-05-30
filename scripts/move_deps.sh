56a6ee9ebf8cf1a10438e9e7f64c741dd53580c0
ci-sourcerer <ci_sourcerer@yahoo.com>
2026-05-29 23:00:49 -0400

    fix: streamline `move_deps.sh`

--- scripts/move_deps.sh
+++ scripts/move_deps.sh
@@ -15,40 +15,41 @@ owner=1000:1000
 mkdir -p $bin_dir "$provider_dir" "$module_dir"
 
 for binary_name in tofu terragrunt; do
-    src_file="$src/$binary_name"
-    dst_file="$bin_dir/$binary_name"
-    if [ -e "$src_file" ]; then
-        rm -rf "$dst_file"
-        mv "$src_file" "$dst_file"
-        chown "$owner" "$dst_file"
-        chmod 755 "$dst_file"
-    fi
+	src_file="$src/$binary_name"
+	dst_file="$bin_dir/$binary_name"
+	if [ -e "$src_file" ]; then
+		rm -rf "$dst_file"
+		mv "$src_file" "$dst_file"
+		chown "$owner" "$dst_file" && chmod 755 "$dst_file"
+	fi
 done
 
-if [ -n "${TERRAFORM_IS_TOFU:-}" ]; then
-    ln -s /usr/local/bin/tofu /usr/local/bin/terraform
-fi
+cat >"$bin_dir/terraform" <<EOF #!/usr/bin/env sh
+printf 'terraform -> tofu\n' >&2
+exec tofu "$@"
+EOF
+chown "$owner" "$bin_dir/terraform" && chmod 755 "$bin_dir/terraform"
 
 src_dir="$src/tofu-providers"
 if [ -d "$src_dir" ]; then
-    for item in "$src_dir"/* "$src_dir"/.[!.]* "$src_dir"/..?*; do
-        [ -e "$item" ] || continue
-        dst="$provider_dir/${item##*/}"
-        rm -rf "$dst"
-        mv "$item" "$dst"
-        chown -R "$owner" "$dst"
-    done
-    rmdir "$src_dir" 2>/dev/null || :
+	for item in "$src_dir"/* "$src_dir"/.[!.]* "$src_dir"/..?*; do
+		[ -e "$item" ] || continue
+		dst="$provider_dir/${item##*/}"
+		rm -rf "$dst"
+		mv "$item" "$dst"
+		chown -R "$owner" "$dst"
+	done
+	rmdir "$src_dir" 2>/dev/null || :
 fi
 
 src_dir="$src/tofu-modules"
 if [ -d "$src_dir" ]; then
-    for item in "$src_dir"/* "$src_dir"/.[!.]* "$src_dir"/..?*; do
-        [ -e "$item" ] || continue
-        dst="$module_dir/${item##*/}"
-        rm -rf "$dst"
-        mv "$item" "$dst"
-        chown -R "$owner" "$dst"
-    done
-    rmdir "$src_dir" 2>/dev/null || :
+	for item in "$src_dir"/* "$src_dir"/.[!.]* "$src_dir"/..?*; do
+		[ -e "$item" ] || continue
+		dst="$module_dir/${item##*/}"
+		rm -rf "$dst"
+		mv "$item" "$dst"
+		chown -R "$owner" "$dst"
+	done
+	rmdir "$src_dir" 2>/dev/null || :
 fi
